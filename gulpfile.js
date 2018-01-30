996d9bf206ad23de8929dd631fb54d7e17627c07
Iván Barcia <ivan@barcia.cc>
2018-01-30 20:05:13 +0100

    Rewrite and optimize all gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,136 +1,210 @@
 // Dependencies
 var gulp         = require('gulp');
 var rename       = require('gulp-rename');
-var notify       = require('gulp-notify');
 var plumber      = require('gulp-plumber');
-var sass         = require('gulp-sass');
+var notify       = require('gulp-notify');
 var sourcemaps   = require('gulp-sourcemaps');
-var sassdoc      = require('sassdoc');
-var concat       = require('gulp-concat');
-var uglify       = require('gulp-uglify-es').default;
+var browserSync  = require('browser-sync').create();
+var pug          = require('gulp-pug');
+var sass         = require('gulp-sass');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano      = require('cssnano');
-var browserSync  = require('browser-sync').create();
+var concat       = require('gulp-concat');
+var uglify       = require('gulp-uglify-es').default;
 var svgstore     = require('gulp-svgstore');
 var svgmin       = require('gulp-svgmin');
 
-// Paths
-var sourcePath = './src/';
-var distPath   = './dist/';
-var docsPath   = './docs/';
 
-// Tasks
-var cssTask         = 'css';
-var jsTask          = 'js';
-var svgTask         = 'svg';
-var docsTask        = 'docs';
-var allTask         = 'all';
-var browserSyncTask = 'sync';
+// Global paths
+const path = {
+	source: 'src/',
+	build: 'build/',
+	dist: 'dist/',
+	docs: 'docs/'
+};
 
 
+// Config
+const config = {
 
-var onError = function (err) {
-  notify({
-    title: 'Gulp: Sass error'
-  }).write(err);
-  console.log(err.toString());
-  this.emit('end');
+	// Sourcemap
+	sourcemap: {
+		build: '../../' + path.build + 'sourcemap'
+	},
+
+	// Browser Sync
+	browsersync: {
+		server: path.dist,
+		// startPath: "index.html",
+		// host: "192.168.0.199",
+		browser: 'google chrome'
+	},
+
+	// HTML
+	html: {
+		task: 'html',
+		source: [
+			path.source + 'pug/**/*.pug',
+			'!' + path.source + 'pug/**/_*.pug',
+		],
+		sourceAll: path.source + 'pug/**/*.pug',
+		dist: path.dist,
+		plugin: {
+			pug: {
+				pretty: true
+			}
+		}
+	},
+
+	// CSS
+	css: {
+		task: 'css',
+		source: path.source + 'scss/**/*.scss',
+		dist: path.dist + 'assets/',
+		plugin: {
+			rename: {
+				suffix: '.min'
+			},
+			sass: {
+
+			},
+			postcss: [
+				autoprefixer({
+					browsers: ['last 2 version']
+				}),
+				cssnano({
+					autoprefixer: false,
+					safe: true,
+					sourcemap: false
+				})
+			]
+		}
+	},
+
+	// JS
+	js: {
+		task: 'js',
+		source: path.source + 'js/**/*.js',
+		dist: path.dist + 'assets/',
+		plugin: {
+			concat: {
+				path: 'script.js'
+			},
+			uglify: {
+
+			},
+			rename: {
+				suffix: '.min'
+			}
+		}
+	},
+
+  // SVG
+	svg: {
+		task: 'svg',
+		source: path.source + 'svg/**/*.svg',
+		dist: path.dist + 'assets/',
+		plugin: {
+			svgmin: {},
+			svgstore: {},
+			rename: {
+				basename: 'icons',
+				suffix: '.min',
+				extname: '.svg'
+			}
+		}
+	}
 };
 
 
 
-// Compile Scss files to CSS, and apply PostCSS plugins
-gulp.task(cssTask, function () {
-  var postcssPlugins = [
-    autoprefixer({
-      browsers: ['last 2 version']
-    }),
-    cssnano({
-      autoprefixer: false,
-      safe: true,
-      sourcemap: false
-    })
-  ];
-
-  var sassOptions = {};
-
-  return gulp.src(sourcePath+'scss/style.scss')
-    .pipe(plumber({ errorHandle: onError }))
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', onError))
-    .pipe(postcss(postcssPlugins))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(distPath));
+// HTML
+gulp.task(config.html.task, function () {
+	return gulp.src(config.html.source)
+	.pipe(plumber({errorHandler: reportError}))
+		.pipe(pug(config.html.plugin.pug))
+	.pipe(plumber.stop())
+	.pipe(gulp.dest(config.html.dist))
+	.pipe(browserSync.stream());
 });
 
 
 
-// Concatenate and minify JS files
-gulp.task(jsTask, function() {
-  return gulp.src(sourcePath+'js/**/*.js')
-    .pipe(concat('script.js'))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(distPath));
+// CSS
+gulp.task(config.css.task, function () {
+	return gulp.src(config.css.source)
+	.pipe(plumber({errorHandler: reportError}))
+		.pipe(sourcemaps.init())
+			.pipe(sass(config.css.plugin.sass))
+			.pipe(postcss(config.css.plugin.postcss))
+			.pipe(rename(config.css.plugin.rename))
+		.pipe(sourcemaps.write(config.sourcemap.build))
+	.pipe(plumber.stop())
+	.pipe(gulp.dest(config.css.dist))
+	.pipe(browserSync.stream());
+});
+
+
+
+// JS
+gulp.task(config.js.task, function() {
+	return gulp.src(config.js.source)
+	.pipe(plumber({errorHandler: reportError}))
+		.pipe(sourcemaps.init())
+			.pipe(concat(config.js.plugin.concat))
+			.pipe(uglify(config.js.plugin.uglify))
+			.pipe(rename(config.js.plugin.rename))
+		.pipe(sourcemaps.write(config.sourcemap.build))
+	.pipe(plumber.stop())
+	.pipe(gulp.dest(config.js.dist))
+	.pipe(browserSync.stream());
 });
 
 
 
 // SVG
-gulp.task(svgTask, function() {
-  return gulp.src(sourcePath+'svg/**/*.svg')
-    .pipe(svgmin())
-    .pipe(svgstore())
-    .pipe(rename({
-      basename: 'icons',
-      suffix: '.min',
-      extname: '.svg'
-    }))
-    .pipe(gulp.dest(distPath));
+gulp.task(config.svg.task, function() {
+	return gulp.src(config.svg.source)
+	.pipe(plumber({errorHandler: reportError}))
+		.pipe(svgmin(config.svg.plugin.svgmin))
+		.pipe(svgstore(config.svg.plugin.svgstore))
+		.pipe(rename(config.svg.plugin.rename))
+	.pipe(plumber.stop())
+	.pipe(gulp.dest(config.svg.dist))
+	.pipe(browserSync.stream());
 });
 
 
 
-// Docs
-gulp.task(docsTask, function () {
-  var options = {
-    dest: docsPath+'reference/',
-  };
-  return gulp.src(sourcePath+'scss/**/*.scss')
-    .pipe(sassdoc(options));
-});
+// ERROR
+var reportError = function (error) {
+	notify({
+		title: 'Gulp error',
+		subtitle: error.plugin,
+		message: error.message,
+		sound: 'Basso',
+		timeout: 3
+	}).write(error);
+	console.error(error.toString());
+	this.emit('end');
+};
 
 
 
-// Watch
+// DEFAULT
 gulp.task('default', function () {
-  gulp.watch(sourcePath+'scss/**/*.scss', [cssTask]);
-  gulp.watch(sourcePath+'js/**/*.js', [jsTask]);
-  gulp.watch(sourcePath+'svg/**/*.svg', [svgTask]);
+
+	// Browser sync
+	browserSync.init(config.browsersync);
+
+	// Watch
+	gulp.watch(config.html.sourceAll, [config.html.task]);
+	gulp.watch(config.css.source, [config.css.task]);
+	gulp.watch(config.js.source, [config.js.task]);
+	gulp.watch(config.svg.source, [config.svg.task]);
 });
 
 
-
-// Browser Sync
-gulp.task(browserSyncTask, function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  });
-
-  gulp.watch(distPath+'style.min.css').on('change', browserSync.reload);
-  gulp.watch(distPath+'script.min.js').on('change', browserSync.reload);
-  gulp.watch(distPath+'icons.min.svg').on('change', browserSync.reload);
-  gulp.watch('*.html').on('change', browserSync.reload);
-});
-
-
-
-gulp.task(allTask, [cssTask, jsTask, svgTask]);
+// All tasks
+gulp.task('all', [config.html.task, config.css.task, config.js.task, config.svg.task]);
