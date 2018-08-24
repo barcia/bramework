d3bcfcc3943ad27fbb67a2dcfc15e1e6e5b79684
Iván Barcia <ivan@barcia.gal>
2018-08-25 01:08:51 +0200

    Add browser.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,4 @@
+const browser =     require('./utils/browser.js');
 const lang =        require('./utils/lang.js');
 const scroll =      require('./utils/scroll.js');
 const time =        require('./utils/time.js');
