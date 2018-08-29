04c214aac33ece9f2f0caa66028fdca89037e6a5
Iván Barcia <ivan@barcia.gal>
2018-08-29 20:04:20 +0200

    Merge data-lang in browser.js object

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,8 @@
 const browser =     require('./utils/browser.js');
-const lang =        require('./utils/lang.js');
 const scroll =      require('./utils/scroll.js');
 const time =        require('./utils/time.js');
 const touchscreen = require('./utils/touchscreen.js');
 const collapse =    require('./components/collapse.js');
 // const dropdown = require('./components/dropdown.js');
 // const visits = require('./utils/visits.js');
+
