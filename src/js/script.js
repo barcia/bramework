6e3c19bcf8991c76f1078f2b6034643905c0cc44
Iván Barcia <ivan@barcia.gal>
2018-09-19 20:43:25 +0200

    Typo

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,3 @@ const scroll =      require('./utils/scroll.js');
 const time =        require('./utils/time.js');
 const touchscreen = require('./utils/touchscreen.js');
 // const visits = require('./utils/visits.js');
-
