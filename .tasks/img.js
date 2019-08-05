a596811580b0db3725dfeb85b3cd397fd062b7ed
Iván Barcia <ivan@barcia.dev>
2019-08-05 13:51:51 +0200

    Change gulp to parcel bundler

--- .tasks/img.js
+++
@@ -1,9 +0,0 @@
-const path = require('./paths.json');
-const gulp = require('gulp');
-
-
-
-module.exports = function() {
-	return gulp.src(path.img.src)
-		.pipe(gulp.dest(path.assets));
-}
--- .tasks/web.js
+++
@@ -1,9 +0,0 @@
-const path = require('./paths.json');
-const gulp = require('gulp');
-
-
-
-module.exports = function() {
-	return gulp.src(path.web.src)
-		.pipe(gulp.dest(path.dist));
-}
